3ed47a38707510463320b3b83fd480e45341c417
Archit Gajjar <gajjararchit00004@gmail.com>
2015-10-19 17:23:00 -0400

    file added.

---
+++ HelloWorldType1.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+public class HelloWorldType1 {
+	public static void main(String[] args) {
+		log("hello world !");
+	}
+	private static void log(String msg){
+		System.out.println(msg);
+	}
+}
